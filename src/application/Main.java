4b99ca46a504f4c1f116930b845cb471632a820a
Diego <diegoateixeira.pro@gmail.com>
2023-08-04 18:34:54 -0300

    fix: implementing the program icon

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,10 +14,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
+			Image icon = new Image(getClass().getResourceAsStream("/resources/icon.png"));
 			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
 			Scene scene = new Scene(parent);
 			stage.setScene(scene);
 			stage.setTitle("Cálculo de hora-extra 50%");
+			stage.getIcons().add(icon);
 			stage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
